9141cf5db9253a6f1c0ee39ca8bdf4f03d77310d
jiwoo1107 <yellow.07.nov@gmail.com>
2024-04-23 17:39:00 +0900

    240423

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,32 +1,68 @@
-var $menu = $('.Menu-list'),
-    $item = $('.Menu-list-item'),
-    w = $(window).width(), //window width
-    h = $(window).height(); //window height
+// resources in description
+const mainTabs = document.querySelector(".main-tabs");
+const mainSliderCircle = document.querySelector(".main-slider-circle");
+const roundButtons = document.querySelectorAll(".round-button");
 
-$(window).on('mousemove', function(e) {
-  var offsetX = 0.5 - e.pageX / w, //cursor position X
-      offsetY = 0.5 - e.pageY / h, //cursor position Y
-      dy = e.pageY - h / 2, //@h/2 = center of poster
-      dx = e.pageX - w / 2, //@w/2 = center of poster
-      theta = Math.atan2(dy, dx), //angle between cursor and center of poster in RAD
-      angle = theta * 180 / Math.PI - 90, //convert rad in degrees
-      offsetPoster = $menu.data('offset'),
-      transformPoster = 'translate3d(0, ' + -offsetX * offsetPoster + 'px, 0) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)'; //poster transform
-
-  //get angle between 0-360
-  if (angle < 0) {
-    angle = angle + 360;
+const colors = {
+  red: {
+    50: {
+      value: "#fff"
+    },
+    100: {
+      value: "#ffcdd2"
+    }
   }
+};
 
-  //poster transform
-  $menu.css('transform', transformPoster);
+const getColor = (color, variant) => {
+  return colors[color][variant].value;
+};
 
-  //parallax for each layer
-  $item.each(function() {
-    var $this = $(this),
-        offsetLayer = $this.data('offset') || 0,
-        transformLayer = 'translate3d(' + offsetX * offsetLayer + 'px, ' + offsetY * offsetLayer + 'px, 20px)';
-
-    $this.css('transform', transformLayer);
+const handleActiveTab = (tabs, event, className) => {
+  tabs.forEach((tab) => {
+    tab.classList.remove(className);
   });
+
+  if (!event.target.classList.contains(className)) {
+    event.target.classList.add(className);
+  }
+};
+
+mainTabs.addEventListener("click", (event) => {
+  const root = document.documentElement;
+  const targetColor = event.target.dataset.color;
+  const targetTranslateValue = event.target.dataset.translateValue;
+
+  if (event.target.classList.contains("round-button")) {
+    mainSliderCircle.classList.remove("animate-jello");
+    void mainSliderCircle.offsetWidth;
+    mainSliderCircle.classList.add("animate-jello");
+
+    root.style.setProperty("--translate-main-slider", targetTranslateValue);
+    root.style.setProperty("--main-slider-color", getColor(targetColor, 50));
+    root.style.setProperty("--background-color", getColor(targetColor, 100));
+
+    handleActiveTab(roundButtons, event, "active");
+
+    if (!event.target.classList.contains("gallery")) {
+      root.style.setProperty("--filters-container-height", "0");
+      root.style.setProperty("--filters-wrapper-opacity", "0");
+    } else {
+      root.style.setProperty("--filters-container-height", "3.8rem");
+      root.style.setProperty("--filters-wrapper-opacity", "1");
+    }
+  }
+});
+
+const filterTabs = document.querySelector(".filter-tabs");
+const filterButtons = document.querySelectorAll(".filter-button");
+
+filterTabs.addEventListener("click", (event) => {
+  const root = document.documentElement;
+  const targetTranslateValue = event.target.dataset.translateValue;
+
+  if (event.target.classList.contains("filter-button")) {
+    root.style.setProperty("--translate-filters-slider", targetTranslateValue);
+    handleActiveTab(filterButtons, event, "filter-active");
+  }
 });
